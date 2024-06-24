d1ef1d953aa45103713fcad923d64f3977191a5b
novel <76740147+novel-code@users.noreply.github.com>
2024-06-25 04:32:56 +0530

    icons and variants page

--- src/AdminLayout.jsx
+++ src/AdminLayout.jsx
@@ -1,11 +1,20 @@
-import { IoHome } from "react-icons/io5";
+import { BiLogOut } from "react-icons/bi";
+import { BsBoxes } from "react-icons/bs";
+import { FaRegGem } from "react-icons/fa";
+import { IoHome, IoMenuOutline, IoSettingsOutline } from "react-icons/io5";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { MdOutlineDashboard } from "react-icons/md";
+import { VscTypeHierarchy } from "react-icons/vsc";
 import { Link, Outlet } from "react-router-dom";
 
 function AdminViewLayout() {
   return (
     <div className='  w-[100%] '>
       <div className='flex h-16 shadow'>
-        <h1 className='flex flex-col justify-center ml-5 text-2xl'>
+        <div className='flex flex-col justify-center ml-5 '>
+          <IoMenuOutline size={26} />
+        </div>
+        <h1 className='flex flex-col justify-center ml-10 text-2xl'>
           Non Stop Traders
         </h1>
         <div className='flex flex-col justify-center ml-auto mr-5 '>
@@ -18,39 +27,48 @@ function AdminViewLayout() {
         <div className='flex flex-col justify-between shadow w-60'>
           <ul className='mt-5 font-medium '>
             <li className='border border-b-0 border-l-0 border-r-0 '>
-              <Link className='p-3' to={"/admin/dashboard"}>
+              <Link className='flex gap-2 p-3' to={"/admin/dashboard"}>
+                <MdOutlineDashboard size={23} />
                 Dashboard
               </Link>
             </li>
             <li className='border border-b-0 border-l-0 border-r-0 '>
-              <Link className='p-3' to={"/admin/categories"}>
+              <Link className='flex gap-2 p-3 ' to={"/admin/categories"}>
+                <BsBoxes size={23} />
                 Catagories
               </Link>
             </li>
             <li className='border border-b-0 border-l-0 border-r-0 '>
-              <Link className='p-3' to={"/admin/variants"}>
+              <Link className='flex gap-2 p-3' to={"/admin/variants"}>
+                <VscTypeHierarchy size={23} />
                 Variants
               </Link>
             </li>
             <li className='border border-b-0 border-l-0 border-r-0 '>
-              <Link className='p-3' to={"/admin/products"}>
+              <Link className='flex gap-2 p-3' to={"/admin/products"}>
+                <FaRegGem size={23} />
                 Products
               </Link>
             </li>
             <li className='border border-l-0 border-r-0 '>
-              <Link className='p-3' to={"/admin/shipping-details"}>
+              <Link className='flex gap-2 p-3' to={"/admin/shipping-details"}>
+                <LiaShippingFastSolid size={23} />
                 Shipping Details
               </Link>
             </li>
           </ul>
           <ul className='mb-5 font-medium'>
             <li className='border border-b-0 border-l-0 border-r-0 '>
-              <Link className='p-3' to={"/admin/settings"}>
+              <Link className='flex gap-2 p-3' to={"/admin/settings"}>
+                <IoSettingsOutline size={23} />
                 Settings
               </Link>
             </li>
             <li className='border border-l-0 border-r-0'>
-              <button className='p-3 pr-[100%] cursor-pointer'>Logout</button>
+              <button className='p-3 flex gap-2 pr-[100%] cursor-pointer'>
+                <BiLogOut size={23} />
+                Logout
+              </button>
             </li>
           </ul>
         </div>
--- src/AdminVariants.jsx
+++ src/AdminVariants.jsx
@@ -1,5 +1,186 @@
+import { useRef, useState } from "react";
+import { IoAddCircle } from "react-icons/io5";
+
 function AdminVariants() {
-  return <div className='text-2xl font-semibold'>Variants</div>;
+  const subVariantInputRef = useRef();
+  const [mainVariants, setMainVariants] = useState([]);
+  const [subVariants, setSubVariants] = useState([]);
+  const [mainVariantInput, setMainVariantInput] = useState("");
+  const [subVariantInput, setSubVariantInput] = useState("");
+  const [selectedMainVariantId, setSelectedMainVariantId] = useState("");
+
+  function handleAddMainVariant() {
+    if (!mainVariantInput) return;
+    setMainVariants((prev) => [
+      ...prev,
+      { name: mainVariantInput, id: crypto.randomUUID() },
+    ]);
+    setMainVariantInput("");
+  }
+
+  function handleFocusSubVariantInput(mainVariantId) {
+    console.log(mainVariantId);
+    subVariantInputRef.current?.focus();
+  }
+  function handleUpdateMainVariant(mainVariantId) {
+    console.log(mainVariantId);
+  }
+  function handleDeleteMainVariant(mainVariantId) {
+    console.log(mainVariantId);
+  }
+
+  const selectedSubVariants = subVariants.filter(
+    (subVariant) => subVariant.mainVariantId === selectedMainVariantId
+  );
+
+  // Sub Variant
+  function handleAddSubVariant() {
+    setSubVariants((prev) => [
+      ...prev,
+      {
+        mainVariantId: selectedMainVariantId,
+        subVariantName: subVariantInput,
+      },
+    ]);
+    setSubVariantInput("");
+  }
+  function handleEditSubVariant() {}
+  function handleDeleteSubVariant() {}
+
+  console.log(subVariants, selectedSubVariants, selectedMainVariantId);
+  return (
+    <div className='max-w-screen-xl bg-green-500l'>
+      <h3 className='text-2xl font-semibold'>Variants</h3>
+      <div className='flex flex-wrap gap-10'>
+        <div className='grow'>
+          <h4 className='mt-12 text-xl font-medium'>Main Variant</h4>
+          <div className='flex gap-2'>
+            <input
+              onChange={(e) => setMainVariantInput(e.currentTarget.value)}
+              value={mainVariantInput}
+              className='p-2 mt-3 border-2 rounded-md min-w-60 w-[80%]'
+              type='text'
+            />
+            <div className='flex flex-col justify-center mt-3'>
+              <IoAddCircle
+                onClick={handleAddMainVariant}
+                size={40}
+                className='cursor-pointer fill-blue-500 hover:fill-blue-600'
+              />
+            </div>
+          </div>
+
+          {mainVariants.length ? (
+            <table className='mt-5 w-[100%]'>
+              <thead>
+                <tr className='text-left '>
+                  <th className='p-2 border-2 min-w-52'>Main Variant Name</th>
+                  <th className='p-2 border-2 '>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {mainVariants.map((variant) => (
+                  <tr
+                    key={variant.id}
+                    className={`${
+                      variant.id === selectedMainVariantId ? "bg-sky-100" : ""
+                    }`}
+                    onClick={() => setSelectedMainVariantId(variant.id)}
+                  >
+                    <td className='p-1 pl-2 border-2 min-w-52'>
+                      {variant.name}
+                    </td>
+                    <td className='p-1 pl-2 border-2'>
+                      <div className='flex flex-wrap gap-1'>
+                        <button
+                          onClick={() => handleFocusSubVariantInput(variant.id)}
+                          className='px-2 py-1 text-sm font-medium bg-green-500 border rounded-md text-green-50 hover:bg-green-600 active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300'
+                        >
+                          Add
+                        </button>
+                        <button
+                          onClick={() => handleUpdateMainVariant(variant.id)}
+                          className='px-2 py-1 text-sm font-medium border rounded-md bg-amber-500 text-amber-50 hover:bg-amber-600 active:bg-amber-700 focus:outline-none focus:ring focus:ring-amber-300 '
+                        >
+                          Edit
+                        </button>
+
+                        <button
+                          onClick={() => handleDeleteMainVariant(variant.id)}
+                          className='px-2 py-1 text-sm font-medium bg-red-500 border rounded-md text-red-50 hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300'
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : null}
+        </div>
+        <div className='grow'>
+          {selectedMainVariantId ? (
+            <>
+              <h4 className='mt-12 text-xl font-medium'>Sub Variant</h4>
+              <div className='flex gap-2'>
+                <input
+                  ref={subVariantInputRef}
+                  className='p-2 mt-3 border-2 rounded-md min-w-60  w-[80%]'
+                  type='text'
+                  onChange={(e) => setSubVariantInput(e.currentTarget.value)}
+                  value={subVariantInput}
+                />
+                <div className='flex flex-col justify-center mt-3'>
+                  <IoAddCircle
+                    onClick={handleAddSubVariant}
+                    size={40}
+                    className='cursor-pointer fill-blue-500 hover:fill-blue-600'
+                  />
+                </div>
+              </div>
+            </>
+          ) : null}
+          {selectedSubVariants?.length ? (
+            <table className='mt-5  w-[100%]'>
+              <thead>
+                <tr className='text-left'>
+                  <th className='p-2 border-2 min-w-52'>Sub Variant Name</th>
+                  <th className='p-2 border-2 '>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedSubVariants.map((subCategory, i) => (
+                  <tr key={i}>
+                    <td className='p-1 pl-2 border-2 min-w-52'>
+                      {subCategory.subVariantName}
+                    </td>
+                    <td className='p-1 pl-2 border-2 '>
+                      <div className='flex flex-wrap gap-1'>
+                        <button
+                          onClick={handleEditSubVariant}
+                          className='px-2 py-1 text-sm font-medium border rounded-md bg-amber-500 text-amber-50 hover:bg-amber-600 active:bg-amber-700 focus:outline-none focus:ring focus:ring-amber-300 '
+                        >
+                          Edit
+                        </button>
+
+                        <button
+                          onClick={handleDeleteSubVariant}
+                          className='px-2 py-1 text-sm font-medium bg-red-500 border rounded-md text-red-50 hover:bg-red-600 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300'
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminVariants;
